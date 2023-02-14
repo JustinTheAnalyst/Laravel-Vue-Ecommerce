d4f7e8c22bd69897b41f5e2f133d18d6cce07ffc
Justin <justin.tys@hotmail.com>
2023-02-15 00:25:59 +1300

    create reset password page

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -2,15 +2,19 @@
   <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="w-full max-w-md space-y-8">
       <div>
-        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Request New Password</h2>
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Set New Password</h2>
         
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
-            <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+            <label for="new-password" class="sr-only">New Password</label>
+            <input id="new-password" name="password" type="password" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="New Password" />
+          </div>
+          <div>
+            <label for="password-repeat" class="sr-only">Repeat Password</label>
+            <input id="password-repeat" name="password_repeat" type="password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Repeat Password" />
           </div>
         </div>
 
