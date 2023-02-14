fece0b7a8d4838b9c35a99c0d2f74452db2dfa3e
Justin <justin.tys@hotmail.com>
2023-02-15 00:09:13 +1300

    create login page

---
+++ backend/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+
+const routes = [
+    {
+        path: "/dashboard",
+        name: "dashbaord",
+        component: Dashboard,
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login,
+    },
+    {
+        path: "/request-password",
+        name: "requestPassword",
+        component: RequestPassword,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
